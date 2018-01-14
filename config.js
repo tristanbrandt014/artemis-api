237d9edd30e9408855ea8ea77d8eba9ba261324b
Tristan Brandt <me@tristanbrandt.co.za>
2018-01-14 14:49:48 +0200

    add captcha on registration

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ export default {
         url: process.env.DB_URL || "mongodb://arte:123456@127.0.0.1:27017/artemis"
     },
     port: process.env.PORT || 3005,
-    captchaSecret: process.env.captchaSecret || "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe" // use fake secret key provided here: https://developers.google.com/recaptcha/docs/faq
+    captchaSecret: process.env.CAPTCHA_SECRET || "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe" // use fake secret key provided here: https://developers.google.com/recaptcha/docs/faq
 }
